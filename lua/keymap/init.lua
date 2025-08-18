8130001952a1dbdee14994d7ee25a32b4958bd79
Gustavo Muniz dos anjos <contact@gustavoanjos.com>
2025-08-18 16:50:18 -0300

    fix(all): Update Lua config and refactor Snacks plugin setup

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -1,21 +1,17 @@
 local M = {}
 
 ---A helper function to create a new keymap.
----@param opts table A table with keymap options.
----@param opts.desc string The description of the keymap.
----@param opts.command string The command or function to execute.
----@param opts.keys table|string A table containing the mode(s) and the key sequence.
----@param opts.options table Optional table for vim.keymap.set.
+---@param opts {desc: string, command: string|function, keys: table, options?: table} A table with keymap options.
 function M.add(opts)
-	local modes, keys = unpack(opts.keys)
-	if type(modes) == 'string' then
-		modes = { modes }
-	end
+    local modes, keys = table.unpack(opts.keys)
+    if type(modes) == 'string' then
+        modes = { modes }
+    end
 
-	local default_opts = { desc = opts.desc, noremap = true, silent = true }
-	local final_opts = vim.tbl_deep_extend("force", default_opts, opts.options or {})
+    local default_opts = { desc = opts.desc, noremap = true, silent = true }
+    local final_opts = vim.tbl_deep_extend("force", default_opts, opts.options or {})
 
-	vim.keymap.set(modes, keys, opts.command, final_opts)
+    vim.keymap.set(modes, keys, opts.command, final_opts)
 end
 
 return M
